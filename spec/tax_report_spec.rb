d7f34b938dcc0abbee07dea756d8cc77c3db49ef
Icelle <icelleku@gmail.com>
2014-01-20 21:45:46 -0500

    spelling

--- spec/tax_report_spec.rb
+++ spec/tax_report_spec.rb
@@ -5,13 +5,13 @@ require_relative '../lib/tax_calculator.rb'
 require_relative '../lib/liability.rb'
 
 describe TaxReport do
-  it 'output employee tax liability' do
+  it 'outputs employee tax liability' do
     employee = Employee.new({first_name: 'Johnny', last_name: 'Smith', annual_income: 120000, tax_paid: 28000, tax_rate: 38})
     tax_report = TaxReport.new(employee)
     expect(employee.liability.amount_owed).to eql(-17600.0)
   end
 
-  it 'output employee refund' do
+  it 'outputs employee refund' do
     employee = Employee.new({first_name: 'Jane', last_name: 'Doe', annual_income: 140000, tax_paid: 30000, tax_rate: 40})
     tax_report = TaxReport.new(employee)
     expect(employee.liability.amount_owed).to eql(-26000.0)
